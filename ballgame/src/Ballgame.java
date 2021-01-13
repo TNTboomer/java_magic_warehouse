ca730f8726cd9b92c61189feefc78f1cb16c5f98
TNTboomer <60656884+TNTboomer@users.noreply.github.com>
2021-01-13 20:55:23 +0800

    Update Ballgame.java

--- ballgame/src/Ballgame.java
+++ ballgame/src/Ballgame.java
@@ -7,9 +7,9 @@ public class Ballgame extends JFrame {
     double x=100;
     double y=100;
     boolean right=true; //true向右
-    //画窗口
+    ////window drawing
     public void paint(Graphics g) {
-        System.out.println("画窗口");
+        System.out.println("window drawing");
         g.drawImage(desk, 0, 0, null);
         g.drawImage(ball, (int) x, (int) y, null);
         if (right) {
@@ -24,7 +24,7 @@ public class Ballgame extends JFrame {
             right = true;
         }
     }
-    //窗口加载
+    ////window loading
     void launchFrame() {
         setSize(856, 500);
         setLocation(50, 50);
@@ -33,7 +33,7 @@ public class Ballgame extends JFrame {
         while (true){
             repaint();
             try{
-                Thread.sleep(16);//1秒60次（约）
+                Thread.sleep(16);////window will be redraw about per 16 ms, 1000ms/16ms is 62.5 FPS.
             }catch (Exception e){
                 e.printStackTrace();
             }
@@ -41,7 +41,7 @@ public class Ballgame extends JFrame {
     }
 
     public static void main(String[] args) {
-        System.out.println("小球游戏");
+        System.out.println("Ballgame");
         Ballgame game = new Ballgame();
         game.launchFrame();
     }
